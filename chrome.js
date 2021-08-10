23a8cfe7d9d9584a1728ee4272fe8b7261dd5cea
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-08-10 20:44:10 +0900

    로그인 구현2

--- chrome.js
+++ chrome.js
@@ -1,10 +1,31 @@
-const loginForm = document.querySelector(".login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "userName";
 
-function onLoginBtnClick() {
-    console.log("hello", loginInput.value);
+//event를 발생시키는 함수는 첫번째 argument로 정보를 받음.
+function onLoginSubmit(event) {
+    event.preventDefault(); //정보의 preventDefault함수를 호출하면 브라우저의 기본 동작을 방지함.
+    const userName = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASS);
+    localStorage.setItem(USERNAME_KEY, userName);
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASS);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+
+
+
+const LocalUserName = localStorage.getItem(USERNAME_KEY);
+
+if(LocalUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASS);
+
+} else {
+    greeting.innerText = `Hello ${LocalUserName}`;
+    greeting.classList.remove(HIDDEN_CLASS);
+}
+
