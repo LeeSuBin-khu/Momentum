17cde6663999ab03376ec5c70335553b2c5a8e30
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-08-11 13:56:56 +0900

    시계 구현2

--- clock.js
+++ clock.js
@@ -2,12 +2,18 @@ const clock = document.querySelector("#clock");
 
 function getTime() {
     const date = new Date();
-    if(date.getSeconds()<10) {
-        clock.innerText = `${date.getHours()}:${date.getMinutes()}:0${date.getSeconds()}`;
-    } else {
-        clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    }
+    const hour = String(date.getHours()).padStart(2, "0"); //쉽게 사용할 수 있는 function이 있는지 찾아보기
+    const min = String(date.getMinutes()).padStart(2, "0");
+    const sec = String(date.getSeconds()).padStart(2, "0");
+
+    clock.innerText = `${hour}:${min}:${sec}`;
+
+    // if(date.getSeconds()<10) {
+    //     clock.innerText = `${date.getHours()}:${date.getMinutes()}:0${date.getSeconds()}`;
+    // } else {
+    //     clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    // }
 }
 
-getTime();
-setInterval(getTime, 1000);
+getTime(); //브라우저를 열자마자 바로 볼 수 있게 하기 위해서
+setInterval(getTime, 1000); //1초마다 함수를 실행
